390c623788e8f4b0d77c6acf7dcb98c6a8882be7
Victor <68400813+Victqr@users.noreply.github.com>
2023-12-05 20:38:13 +0100

    Update main.py

--- codes/main.py
+++ codes/main.py
@@ -1,1 +1,50 @@
+""" Transmitter BMP280, TMP36, RFM69 """
 
+from machine import SPI, I2C, Pin, ADC
+from rfm69 import RFM69
+from bme280 import BME280, BMP280_I2CADDR
+import time
+
+NAME           = "data from SpaceSence/Kers_SAT"
+FREQ           = 433.8
+
+ENCRYPTION_KEY = b"\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08"
+NODE_ID        = 120 # ID of this node
+BASESTATION_ID = 100 # ID of the node (base station) to be contacted
+
+# Buses & Pins
+spi = SPI(0, baudrate=50000, polarity=0, phase=0, firstbit=SPI.MSB)
+nss = Pin( 5, Pin.OUT, value=True )
+rst = Pin( 3, Pin.OUT, value=False )
+i2c = I2C(0)
+
+# RFM Module
+rfm = RFM69( spi=spi, nss=nss, reset=rst )
+rfm.frequency_mhz  = FREQ
+rfm.encryption_key = ( ENCRYPTION_KEY )
+rfm.node           = NODE_ID # This instance is the node 120
+rfm.destination    = BASESTATION_ID # Send to specific node 100
+
+bmp = BME280(i2c=i2c, address=BMP280_I2CADDR) # create a bmp object
+adc = ADC(Pin(26)) # TMP36 analog pin
+led = Pin(25, Pin.OUT) # Onboard LED
+
+# Main Loop
+print( 'Frequency     :', rfm.frequency_mhz )
+print( 'encryption    :', rfm.encryption_key )
+print( 'NODE_ID       :', NODE_ID )
+print( 'BASESTATION_ID:', BASESTATION_ID )
+
+print("iteration_count, time_sec, pressure_hpa, bmp280_temp") # print header
+counter = 1 # set counter
+ctime = time.time() # time now
+
+while True:
+    temp, pressure, humidity =  bmp.raw_values # read BMP280: Temp, pressure (hPa), humidity
+    msg = f"{counter}, {time.time()-ctime}, {pressure:.2f}, {temp:.2f}, {NAME}"
+    print(msg)
+    led.on() # Led ON while sending data
+    rfm.send(bytes(msg , "utf-8"))
+    led.off()
+    counter += 1 # increment counter
+    time.sleep(0.5) # wait before next reading
